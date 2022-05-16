eb35e936dc9ba18f3411d1dd51e6d3b76f9be739
Jackie <jye0325@gmail.com>
2022-05-16 12:15:47 -0400

    commit tree 04d58af3a14d6db21c33115d89d4fccc8f4ff42c

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+// Multiply
+let num1 = 31;
+let num2 = 2;
+const multiply = num1*num2;
+
+// Random
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Mod -- Remainder
+let num3 = 4;
+let num4 = 5;
+const mod = num3%num4;
+
+// Max
+const max = Math.max(1, 2, 4, 5, 20)
